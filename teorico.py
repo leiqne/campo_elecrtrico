85b7ab797e77a599c908b790262fc21d253742c1
Freddy Alejandro ☯️ <134672902+Alexandre0311@users.noreply.github.com>
2023-10-02 00:43:01 -0500

    Metodo_teorico.py
    Comentarios

--- teorico.py
+++ teorico.py
@@ -1,24 +1,20 @@
-
+#------------------Ecuación Teorica-------------------
 import matplotlib.pyplot as plt
 import math
 
-
-
 k=9.00*pow(10,9)
-
 permitiVacio=8.85*pow(10,-12)
 densidad=20.0
-
 CampoE=[]
+
 for x in range (100):
     if x>=2:
         operacion=(densidad/(2*math.pi*permitiVacio*x))
         CampoE.append(operacion)
-
-    
-
+        
 x=range(2,100)
 
+#--------------------GRAFICA--------------------
 # Crear la gráfica
 plt.figure(figsize=(8, 6))  # Tamaño de la figura
 
