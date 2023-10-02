83162528afe134f5ce0b52b79ec2c6f755a30c0f
mauricio castilla eguia <joaquincastillaeguia7@gmail.com>
2023-10-02 00:11:03 -0500

    se agrego el calculo nuemerico y teorico por separado

---
+++ numerico.py
@@ -0,0 +1,35 @@
+
+import matplotlib.pyplot as plt
+import math
+k=9.00*pow(10,9)
+densidad=20.0
+yi=0.02
+qi=densidad*yi
+
+E=[]
+
+for x in range(100):
+
+    if x>=2:
+        distancia=math.sqrt(((x*yi)**2+x**2))
+        trapecio=(2.00*k*qi/(distancia**2))*x         #x=cos porque es el eje horizontal del vector r (ver figura)
+        E.append(trapecio)
+
+x=range(2,100)
+
+# Crear la gráfica
+plt.figure(figsize=(8, 6))  # Tamaño de la figura
+
+# Agregar los datos a la gráfica
+plt.plot(x, E, marker='o')
+
+# Etiquetas de los ejes
+plt.xlabel('X (m)')
+plt.ylabel('E (N/C)')
+
+# Título de la gráfica
+plt.title('Grafico E (N/C) vs X (m)')
+
+# Mostrar la gráfica
+plt.grid(True)  # Mostrar cuadrícula
+plt.show()
---
+++ teorico.py
@@ -0,0 +1,37 @@
+
+import matplotlib.pyplot as plt
+import math
+
+
+
+k=9.00*pow(10,9)
+
+permitiVacio=8.85*pow(10,-12)
+densidad=20.0
+
+CampoE=[]
+for x in range (100):
+    if x>=2:
+        operacion=(densidad/(2*math.pi*permitiVacio*x))
+        CampoE.append(operacion)
+
+    
+
+x=range(2,100)
+
+# Crear la gráfica
+plt.figure(figsize=(8, 6))  # Tamaño de la figura
+
+# Agregar los datos a la gráfica
+plt.plot(x, CampoE, marker='o')
+
+# Etiquetas de los ejes
+plt.xlabel('X (m)')
+plt.ylabel('E (N/C)')
+
+# Título de la gráfica
+plt.title('Grafico E (N/C) vs X (m)')
+
+# Mostrar la gráfica
+plt.grid(True)  # Mostrar cuadrícula
+plt.show()
